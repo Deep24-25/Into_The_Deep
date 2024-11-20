9f5e159b9c90bda90389f330a2dcb7a925c35740
Vivaan S. <72049995+Vivaan-crypto@users.noreply.github.com>
2024-11-19 22:27:43 -0500

    ArmFSM tests done.

---
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/Teleop/Monkeys_Limb/ArmFSMTest.java
@@ -0,0 +1,91 @@
+package org.firstinspires.ftc.teamcode.Teleop.Monkeys_Limb;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+import org.firstinspires.ftc.teamcode.Core.HWMap;
+import org.firstinspires.ftc.teamcode.Teleop.ArmMotorsWrapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ArmFSMTest {
+    private ArmFSM sut;
+    private ArmMotorsWrapper armMotorsWrapperMock = mock();
+    private HWMap hwMap = mock();
+    @BeforeEach
+    public void setup(){
+        sut = spy(new ArmFSM(hwMap, armMotorsWrapperMock));
+    }
+    /**---------------------------updateState()---------------------------------**/
+    @Test
+    public void FULLY_RETRACTED(){
+        doReturn(true).when(sut.atPos(sut.getTolerance()));
+        doReturn(true).when(sut.isTargetPosAtFullyRetractedHeight());
+
+        sut.updateState();
+
+        verify(armMotorsWrapperMock).readPositionInCM();
+        assertTrue(sut.FULLY_RETRACTED());
+    }
+    @Test
+    public void AT_BASKET_HEIGHT(){
+        doReturn(true).when(sut.atPos(sut.getTolerance()));
+        doReturn(false).when(sut.isTargetPosAtFullyRetractedHeight());
+        doReturn(true).when(sut.isTargetPosAtBasketHeight());
+
+        sut.updateState();
+
+        verify(armMotorsWrapperMock).readPositionInCM();
+        assertTrue(sut.AT_BASKET_HEIGHT());
+    }
+    @Test
+    public void AT_SUBMERSIBLE_HEIGHT(){
+        doReturn(true).when(sut.atPos(sut.getTolerance()));
+        doReturn(false).when(sut.isTargetPosAtFullyRetractedHeight());
+        doReturn(false).when(sut.isTargetPosAtBasketHeight());
+        doReturn(true).when(sut.isTargetPosAtSubmersibleHeight());
+
+        sut.updateState();
+
+        verify(armMotorsWrapperMock).readPositionInCM();
+        assertTrue(sut.AT_SUBMERSIBLE_HEIGHT());
+    }
+    @Test
+    public void FULLY_EXTENDED(){
+        doReturn(true).when(sut.atPos(sut.getTolerance()));
+        doReturn(false).when(sut.isTargetPosAtFullyRetractedHeight());
+        doReturn(false).when(sut.isTargetPosAtBasketHeight());
+        doReturn(false).when(sut.isTargetPosAtSubmersibleHeight());
+
+        sut.updateState();
+
+        verify(armMotorsWrapperMock).readPositionInCM();
+        assertTrue(sut.FULLY_EXTENDED());
+    }
+    @Test
+    public void MOVING_ABOVE_SAFE_HEIGHT(){
+        doReturn(false).when(sut.atPos(sut.getTolerance()));
+        doReturn(true).when(sut.isTargetPosAboveSafeHeight());
+
+        sut.updateState();
+
+        verify(armMotorsWrapperMock).readPositionInCM();
+        verify(armMotorsWrapperMock).getLastReadPositionInCM();
+        assertTrue(sut.MOVING_ABOVE_SAFE_HEIGHT());
+    }
+    @Test
+    public void MOVING_BELOW_SAFE_HEIGHT(){
+        doReturn(false).when(sut.atPos(sut.getTolerance()));
+        doReturn(false).when(sut.isTargetPosAboveSafeHeight());
+        doReturn(true).when(sut.isTargetPosBelowSafeHeight());
+
+        sut.updateState();
+
+        verify(armMotorsWrapperMock).readPositionInCM();
+        verify(armMotorsWrapperMock).getLastReadPositionInCM();
+        assertTrue(sut.MOVING_BELOW_SAFE_HEIGHT());
+    }
+}
--- TeamCode/src/test/java/org/firstinspires/ftc/teamcode/Teleop/Monkeys_Limb/LimbFSMTest.java
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/Teleop/Monkeys_Limb/LimbFSMTest.java
@@ -27,4 +27,29 @@ class LimbFSMTest {
 
         assertTrue(sut.PREPARING_TO_INTAKE_SPECIMEN());
     }
+    @Test
+    public void intakingSpecimenState(){
+        sut.findTargetState(true, Mockito.anyBoolean(), Mockito.anyBoolean(), Mockito.anyBoolean());
+        doReturn(true).when(sut.SPECIMEN_MODE());
+        doReturn(true).when(sut.PREPARED_TO_INTAKE_SPECIMEN());
+
+        assertTrue(sut.INTAKING_SPECIMEN());
+    }
+    @Test
+    public void extendingToSpecimenState(){
+        sut.findTargetState(true, Mockito.anyBoolean(), Mockito.anyBoolean(), Mockito.anyBoolean());
+        doReturn(true).when(sut.SPECIMEN_MODE());
+        doReturn(true).when(sut.INTAKED_SPECIMEN());
+
+        assertTrue(sut.EXTENDING_TO_SPECIMEN());
+    }
+    @Test
+    public void depositingSpecimenState(){
+        sut.findTargetState(true, Mockito.anyBoolean(), Mockito.anyBoolean(), Mockito.anyBoolean());
+        doReturn(true).when(sut.SPECIMEN_MODE());
+        doReturn(true).when(sut.EXTENDED_SPECIMEN());
+
+        assertTrue(sut.EXTENDING_TO_SPECIMEN());
+    }
+    
 }
