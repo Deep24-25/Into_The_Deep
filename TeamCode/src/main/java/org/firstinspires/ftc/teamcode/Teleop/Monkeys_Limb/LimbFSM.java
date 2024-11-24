ec1d2161e2b6f5527f387217090923b85aeaf899
Vivaan S. <72049995+Vivaan-crypto@users.noreply.github.com>
2024-11-24 08:25:01 -0500

    Most tests complete.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Monkeys_Limb/LimbFSM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Monkeys_Limb/LimbFSM.java
@@ -1,21 +1,21 @@
 package org.firstinspires.ftc.teamcode.Teleop.Monkeys_Limb;
 
-import org.firstinspires.ftc.teamcode.Core.HWMap;
+import androidx.annotation.VisibleForTesting;
 
 public class LimbFSM {
     public enum States{
-        PREPARING_TO_INTAKE_SPECIMEN, PREPARED_TO_INTAKE_SPECIMEN, INTAKING_SPECIMEN, INTAKED_SPECIMEN, EXTENDING_TO_SPECIMEN, EXTENDED_SPECIMEN, DEPOSITING_SPECIMEN, DEPOSITED_SPECIMEN, PREPARING_TO_DEPOSIT_SPECIMEN, PREPARED_TO_DEPOSIT_SPECIMEN, EXTENDING_TO_BASKET_HEIGHT, EXTENDED_TO_BASKET_HEIGHT, PREPARING_TO_INTAKE, PREPARED_TO_INTAKE, MOVING_TO_INTAKE_POS, MOVED_TO_INTAKE_POS, MOVING_TO_MINI_INTAKE, MOVED_TO_MINI_INTAKE, RETRACTING_FROM_MINI_INTAKE, RETRACTED_FROM_MINI_INTAKE
+        PREPARING_TO_INTAKE_SPECIMEN, PREPARED_TO_INTAKE_SPECIMEN, INTAKING_SPECIMEN, INTAKED_SPECIMEN, EXTENDING_TO_SPECIMEN, EXTENDED_SPECIMEN, DEPOSITING_SPECIMEN, DEPOSITED_SPECIMEN, PREPARING_TO_DEPOSIT_SPECIMEN, PREPARED_TO_DEPOSIT_SPECIMEN, PREPARING_TO_DEPOSIT_SAMPLE, PREPARED_TO_DEPOSIT_SAMPLE, EXTENDING_TO_BASKET_HEIGHT, EXTENDED_TO_BASKET_HEIGHT, DEPOSITING_SAMPLE, DEPOSITED_SAMPLE, PREPARING_TO_INTAKE, PREPARED_TO_INTAKE, MOVING_TO_INTAKE_POS, MOVED_TO_INTAKE_POS, MOVING_TO_MINI_INTAKE, MOVED_TO_MINI_INTAKE, RETRACTING_FROM_MINI_INTAKE, RETRACTED_FROM_MINI_INTAKE
     }
     public enum Mode{
         SAMPLE_MODE, SPECIMEN_MODE
     }
 
-    private States states = States.INTAKED_SPECIMEN;
-    private Mode mode = Mode.SAMPLE_MODE;
+    private States currentState = States.INTAKED_SPECIMEN;
+    private Mode currentMode = Mode.SAMPLE_MODE;
     public LimbFSM(){
 
     }
-    public void findTargetState(boolean yPressed, boolean aPressed, boolean rightBumperPressed, boolean leftBumperPressed){
+    public void findTargetState(boolean yPressed, boolean aPressed, boolean xPressed, boolean rbPressed, boolean lbPressed,     boolean rightBumperPressed, boolean leftBumperPressed){
 
     }
     public void updateState(){
@@ -23,72 +23,91 @@ public class LimbFSM {
     }
 
     public boolean PREPARING_TO_INTAKE_SPECIMEN(){
-        return states == States.PREPARING_TO_INTAKE_SPECIMEN;
+        return currentState == States.PREPARING_TO_INTAKE_SPECIMEN;
     }
     public boolean PREPARED_TO_INTAKE_SPECIMEN(){
-        return states == States.PREPARED_TO_INTAKE_SPECIMEN;
+        return currentState == States.PREPARED_TO_INTAKE_SPECIMEN;
     }
     public boolean INTAKING_SPECIMEN(){
-        return states == States.INTAKING_SPECIMEN;
+        return currentState == States.INTAKING_SPECIMEN;
     }
     public boolean INTAKED_SPECIMEN(){
-        return states == States.INTAKED_SPECIMEN;
+        return currentState == States.INTAKED_SPECIMEN;
     }
     public boolean EXTENDING_TO_SPECIMEN(){
-        return states == States.EXTENDING_TO_SPECIMEN;
+        return currentState == States.EXTENDING_TO_SPECIMEN;
     }
     public boolean EXTENDED_SPECIMEN(){
-        return states == States.EXTENDED_SPECIMEN;
+        return currentState == States.EXTENDED_SPECIMEN;
     }
     public boolean DEPOSITING_SPECIMEN(){
-        return states == States.DEPOSITING_SPECIMEN;
+        return currentState == States.DEPOSITING_SPECIMEN;
     }
     public boolean DEPOSITED_SPECIMEN(){
-        return states == States.DEPOSITED_SPECIMEN;
+        return currentState == States.DEPOSITED_SPECIMEN;
     }
     public boolean PREPARING_TO_DEPOSIT_SPECIMEN(){
-        return states == States.PREPARING_TO_DEPOSIT_SPECIMEN;
+        return currentState == States.PREPARING_TO_DEPOSIT_SPECIMEN;
     }
     public boolean PREPARED_TO_DEPOSIT_SPECIMEN(){
-        return states == States.PREPARED_TO_DEPOSIT_SPECIMEN;
+        return currentState == States.PREPARED_TO_DEPOSIT_SPECIMEN;
+    }
+    public boolean PREPARING_TO_DEPOSIT_SAMPLE(){
+        return currentState == States.PREPARING_TO_DEPOSIT_SAMPLE;
+    }
+    public boolean PREPARED_TO_DEPOSIT_SAMPLE(){
+        return currentState == States.PREPARED_TO_DEPOSIT_SAMPLE;
     }
     public boolean EXTENDING_TO_BASKET_HEIGHT(){
-        return states == States.EXTENDING_TO_BASKET_HEIGHT;
+        return currentState == States.EXTENDING_TO_BASKET_HEIGHT;
     }
     public boolean EXTENDED_TO_BASKET_HEIGHT(){
-        return states == States.EXTENDED_TO_BASKET_HEIGHT;
+        return currentState == States.EXTENDED_TO_BASKET_HEIGHT;
+    }
+    public boolean DEPOSITING_SAMPLE(){
+        return currentState == States.DEPOSITING_SAMPLE;
+    }
+    public boolean DEPOSITED_SAMPLE(){
+        return currentState == States.DEPOSITED_SAMPLE;
     }
     public boolean PREPARING_TO_INTAKE(){
-        return states == States.PREPARING_TO_INTAKE;
+        return currentState == States.PREPARING_TO_INTAKE;
     }
 
     public boolean PREPARED_TO_INTAKE(){
-        return states == States.PREPARED_TO_INTAKE;
+        return currentState == States.PREPARED_TO_INTAKE;
     }
     public boolean MOVING_TO_INTAKE_POS(){
-        return states == States.MOVING_TO_INTAKE_POS;
+        return currentState == States.MOVING_TO_INTAKE_POS;
     }
     public boolean MOVED_TO_INTAKE_POS(){
-        return states == States.MOVED_TO_INTAKE_POS;
+        return currentState == States.MOVED_TO_INTAKE_POS;
     }
     public boolean MOVING_TO_MINI_INTAKE(){
-        return states == States.MOVING_TO_MINI_INTAKE;
+        return currentState == States.MOVING_TO_MINI_INTAKE;
     }
     public boolean MOVED_TO_MINI_INTAKE(){
-        return states == States.MOVED_TO_MINI_INTAKE;
+        return currentState == States.MOVED_TO_MINI_INTAKE;
     }
     public boolean RETRACTING_FROM_MINI_INTAKE(){
-        return states == States.RETRACTING_FROM_MINI_INTAKE;
+        return currentState == States.RETRACTING_FROM_MINI_INTAKE;
     }
     public boolean RETRACTED_FROM_MINI_INTAKE(){
-        return states == States.RETRACTED_FROM_MINI_INTAKE;
+        return currentState == States.RETRACTED_FROM_MINI_INTAKE;
     }
     public boolean SAMPLE_MODE(){
-        return mode == Mode.SAMPLE_MODE;
+        return currentMode == Mode.SAMPLE_MODE;
     }
     public boolean SPECIMEN_MODE(){
-        return mode == Mode.SPECIMEN_MODE;
+        return currentMode == Mode.SPECIMEN_MODE;
     }
-
+    @VisibleForTesting
+    public void setCurrentState(States state) {
+        currentState = state;
+    }
+    @VisibleForTesting
+    public void setCurrentMode(Mode mode) {
+        this.currentMode = mode;
+    }
 
 }
--- TeamCode/src/test/java/org/firstinspires/ftc/teamcode/Teleop/Monkeys_Limb/LimbFSMTest.java
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/Teleop/Monkeys_Limb/LimbFSMTest.java
@@ -19,37 +19,166 @@ class LimbFSMTest {
     /**
      * ------------------------------------findTargetState()-----------------------------------
      **/
+
+    // Specimen State Tests
     @Test
     public void preparingToIntakeSpecimenState() {
-        sut.findTargetState(true, Mockito.anyBoolean(), Mockito.anyBoolean(), Mockito.anyBoolean());
         doReturn(true).when(sut.SPECIMEN_MODE());
         doReturn(true).when(sut.PREPARED_TO_INTAKE());
+
+        sut.findTargetState(true, false, false, false, false, false, false);
 
         assertTrue(sut.PREPARING_TO_INTAKE_SPECIMEN());
     }
     @Test
     public void intakingSpecimenState(){
-        sut.findTargetState(true, Mockito.anyBoolean(), Mockito.anyBoolean(), Mockito.anyBoolean());
         doReturn(true).when(sut.SPECIMEN_MODE());
         doReturn(true).when(sut.PREPARED_TO_INTAKE_SPECIMEN());
+
+        sut.findTargetState(true, false, false, false, false, false, false);
 
         assertTrue(sut.INTAKING_SPECIMEN());
     }
     @Test
     public void extendingToSpecimenState(){
-        sut.findTargetState(true, Mockito.anyBoolean(), Mockito.anyBoolean(), Mockito.anyBoolean());
         doReturn(true).when(sut.SPECIMEN_MODE());
         doReturn(true).when(sut.INTAKED_SPECIMEN());
+
+        sut.findTargetState(true, false, false, false, false, false, false);
 
         assertTrue(sut.EXTENDING_TO_SPECIMEN());
     }
     @Test
     public void depositingSpecimenState(){
-        sut.findTargetState(true, Mockito.anyBoolean(), Mockito.anyBoolean(), Mockito.anyBoolean());
         doReturn(true).when(sut.SPECIMEN_MODE());
         doReturn(true).when(sut.EXTENDED_SPECIMEN());
 
+        sut.findTargetState(true, false,  false, false, false,false, false);
+
         assertTrue(sut.EXTENDING_TO_SPECIMEN());
     }
-    
+    @Test
+    public void intakingSpecimenToPreparingToIntakeSpecimenState(){
+        doReturn(true).when(sut.SPECIMEN_MODE());
+        doReturn(true).when(sut.INTAKING_SPECIMEN());
+
+        sut.findTargetState(false, false,  true, false, false,false, false);
+
+        assertTrue(sut.PREPARING_TO_INTAKE_SPECIMEN());
+    }
+    @Test
+    public void intakedSpecimenToPreparingToIntakeSpecimenState(){
+        doReturn(true).when(sut.SPECIMEN_MODE());
+        doReturn(true).when(sut.INTAKED_SPECIMEN());
+
+        sut.findTargetState(false, false,  true, false, false,false, false);
+
+        assertTrue(sut.PREPARING_TO_INTAKE_SPECIMEN());
+    }
+    @Test
+    public void extendingToSpecimenToIntakingSpecimenState(){
+        doReturn(true).when(sut.SPECIMEN_MODE());
+        doReturn(true).when(sut.EXTENDING_TO_SPECIMEN());
+
+        sut.findTargetState(false, false,  true, false, false,false, false);
+
+        assertTrue(sut.INTAKING_SPECIMEN());
+    }
+    @Test
+    public void extendedToSpecimenToIntakingSpecimenState(){
+        doReturn(true).when(sut.SPECIMEN_MODE());
+        doReturn(true).when(sut.EXTENDED_SPECIMEN());
+
+        sut.findTargetState(false, false,  true, false, false,false, false);
+
+        assertTrue(sut.INTAKING_SPECIMEN());
+    }
+    @Test
+    public void depositingSpecimenToExtendingToSpecimenState(){
+        doReturn(true).when(sut.SPECIMEN_MODE());
+        doReturn(true).when(sut.DEPOSITING_SPECIMEN());
+
+        sut.findTargetState(false, false,  true, false, false,false, false);
+
+        assertTrue(sut.EXTENDING_TO_SPECIMEN());
+    }
+
+    @Test
+    public void depositedSpecimenAndYPressed(){
+        doReturn(true).when(sut.SPECIMEN_MODE());
+        doReturn(true).when(sut.DEPOSITED_SPECIMEN());
+
+        sut.findTargetState(true, false,  false, false, false,false, false);
+
+        assertTrue(sut.PREPARING_TO_INTAKE_SPECIMEN());
+
+    }
+    @Test
+    public void depositedSpecimenAndAPressed(){
+        doReturn(true).when(sut.SPECIMEN_MODE());
+        doReturn(true).when(sut.DEPOSITED_SPECIMEN());
+
+        sut.findTargetState(false, true,  false, false, false,false, false);
+
+        assertTrue(sut.PREPARING_TO_INTAKE());
+
+    }
+    //Sample State Tests
+    @Test
+    public void preparingToDepositStateAndNotDepositedSample(){
+        doReturn(true).when(sut.DEPOSITED_SAMPLE());
+        doReturn(true).when(sut.SAMPLE_MODE());
+
+        sut.findTargetState(true, false,  false, false, false,false, false);
+
+        assertTrue(sut.PREPARING_TO_DEPOSIT_SAMPLE());
+
+    }
+    @Test
+    public void preparingToDepositStateAndNotPreparedToDepositSampleAndNotDepositingSample(){
+        doReturn(false).when(sut.PREPARED_TO_DEPOSIT_SAMPLE());
+        doReturn(false).when(sut.DEPOSITING_SAMPLE());
+        doReturn(true).when(sut.SAMPLE_MODE());
+
+        sut.findTargetState(true, false,  false, false, false,false, false);
+
+        assertTrue(sut.PREPARING_TO_DEPOSIT_SAMPLE());
+
+    }
+    @Test
+    public void extendingToBasketHeight(){
+        doReturn(true).when(sut.PREPARED_TO_DEPOSIT_SAMPLE());
+        doReturn(true).when(sut.SAMPLE_MODE());
+
+        sut.findTargetState(true, false,  false, false, false,false, false);
+
+        assertTrue(sut.EXTENDING_TO_BASKET_HEIGHT());
+
+    }
+    @Test
+    public void depositingSample(){
+        doReturn(true).when(sut.EXTENDED_TO_BASKET_HEIGHT());
+        doReturn(true).when(sut.SAMPLE_MODE());
+
+        sut.findTargetState(true, false,  false, false, false,false, false);
+
+        assertTrue(sut.DEPOSITING_SAMPLE());
+    }
+    //Intake States
+    @Test
+    public void preparingToIntakeAndNotPreparedToIntake(){
+        doReturn(false).when(sut.PREPARED_TO_INTAKE());
+
+        sut.findTargetState(false, true,  false, false, false,false, false);
+
+        assertTrue(sut.PREPARING_TO_INTAKE());
+    }
+    @Test
+    public void preparingToIntakeAndNotMovingToIntakePos(){
+        doReturn(false).when(sut.PREPARED_TO_INTAKE());
+
+        sut.findTargetState(false, true,  false, false, false,false, false);
+
+        assertTrue(sut.PREPARING_TO_INTAKE());
+    }
 }
